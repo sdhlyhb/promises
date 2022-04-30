471270d39243cc4a3671cb64df20c21f755c05b1
Danting Huang <serena.huangdanting@gmail.com>
2022-04-30 00:53:46 -0400

    attempted collection.js

--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -7,10 +7,22 @@
  *      - i.e. the second line in the new file should be the first line of `filePaths[1]`
  *    4. Writes the new file to the file located at `writePath`
  */
-
+var fs = require('fs');
+const Promise = require('bluebird');
+const fsPromisify = Promise.promisifyAll(fs);
+var promisification = require('../bare_minimum/promisification');
+var promiseConstructor = require('../bare_minimum/promiseConstructor');
 
 var combineFirstLineOfManyFiles = function(filePaths, writePath) {
   // TODO
+  return Promise.all(filePaths.map(filePath => {
+    return promiseConstructor.pluckFirstLineFromFileAsync(filePath)
+      .then ((firstLine) => fsPromisify.writeFileAsync(writePath, firstLine + '\n', 'utf8'));
+
+  })).then ((data) => console.log('success!'))
+    .catch(err => console.log(err));
+
+
 };
 
 // Export these functions so we can unit test them
